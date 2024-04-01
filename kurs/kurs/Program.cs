513ea43508b696223de909166de713ab8dc69db3
Rus2302 <127191339+Rus2302@users.noreply.github.com>
2024-04-01 20:14:01 +0300

    add

--- kurs/kurs/Program.cs
+++ kurs/kurs/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace TestC;
+﻿namespace kurs;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello!");
+        Console.WriteLine("Hello, World!");
     }
 }
 
